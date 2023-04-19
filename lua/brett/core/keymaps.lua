adc6310a740fd7ab8f13b39951865d454106b988
Brett Smith <brettsmith212@gmail.com>
2023-04-18 22:31:53 -0700

    new keymaps added

--- lua/brett/core/keymaps.lua
+++ lua/brett/core/keymaps.lua
@@ -8,8 +8,9 @@ keymap.set("i", "jk", "<Esc>")
 
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
-keymap.set("n", "<leader>+", "<C-a>")
-keymap.set("n", "<leader>-", "<C-x>")
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+keymap.set("n", "<C-a>", "gg<S-v>G") -- select all
 
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
@@ -17,6 +18,7 @@ keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current window split
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>te", ":tabedit<CR>", { silent = true }) -- new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to prev tab
