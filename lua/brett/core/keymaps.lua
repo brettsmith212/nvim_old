991497c72212be9a3bb25ef7fc6085d3c6e92347
Brett Smith <brettsmith212@gmail.com>
2023-04-21 23:53:21 -0700

    fix debug run buttons

--- lua/brett/core/keymaps.lua
+++ lua/brett/core/keymaps.lua
@@ -63,9 +63,9 @@ keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 -- debugger
 keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
-keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>")
-keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>")
-keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+keymap.set("n", "<F6>", ":lua require'dap'.step_over()<CR>")
+keymap.set("n", "<F7>", ":lua require'dap'.step_into()<CR>")
+keymap.set("n", "<F8>", ":lua require'dap'.step_out()<CR>")
 keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
 keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 keymap.set("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
